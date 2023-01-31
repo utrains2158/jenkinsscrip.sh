48ba81fd6ce137a24eb4cd8254584b0007491a44
Samka1 <utrians.@outlook.com>
2023-01-30 23:39:43 -0600

    add command wget

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -15,10 +15,7 @@ systemctl start docker
 usermod -aG docker ubuntu
 
 ## Extra packages install 
-apt install vim git -Y
-
-## Extra packages install
-apt install wget git -Y
+apt install vim git wget -Y
 
 
 # run jenkins
